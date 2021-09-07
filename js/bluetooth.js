290321ea0c25a4028b674749f62495c9f72f19f9
Hemmy Gerrits <HemmyGerrits@gmail.com>
2021-09-07 12:01:32 -0700

    f

---
+++ js/bluetooth.js
@@ -0,0 +1,67 @@
+var bluetoothDevice;
+
+function onButtonClick() {
+  bluetoothDevice = null;
+  debugLog('Requesting any Bluetooth Device...');
+  navigator.bluetooth.requestDevice({
+     // filters: [...] <- Prefer filters to save energy & show relevant devices.
+     acceptAllDevices: true})
+  .then(device => {
+    bluetoothDevice = device;
+    bluetoothDevice.addEventListener('gattserverdisconnected', onDisconnected);
+    connect();
+  })
+  .catch(error => {
+    debugLog('Argh! ' + error);
+  });
+}
+
+function connect() {
+  exponentialBackoff(3 /* max retries */, 2 /* seconds delay */,
+    function toTry() {
+      time('Connecting to Bluetooth Device... ');
+      return bluetoothDevice.gatt.connect();
+    },
+    function success() {
+      debugLog('> Bluetooth Device connected. Try disconnect it now.');
+	  
+let txt = "";
+			for (let x in bluetoothDevice) {
+			txt += bluetoothDevice[x] + " ";
+}; debugLog(txt);
+    },
+    function fail() {
+      time('Failed to reconnect.');
+    });
+}
+
+function onDisconnected() {
+  debugLog('> Bluetooth Device disconnected');
+  connect();
+}
+
+/* Utils */
+
+// This function keeps calling "toTry" until promise resolves or has
+// retried "max" number of times. First retry has a delay of "delay" seconds.
+// "success" is called upon success.
+function exponentialBackoff(max, delay, toTry, success, fail) {
+  toTry().then(result => success(result))
+  .catch(_ => {
+    if (max === 0) {
+      return fail();
+    }
+    time('Retrying in ' + delay + 's... (' + max + ' tries left)');
+    setTimeout(function() {
+      exponentialBackoff(--max, delay * 2, toTry, success, fail);
+    }, delay * 1000);
+  });
+}
+
+function time(text) {
+  debugLog('[' + new Date().toJSON().substr(11, 8) + '] ' + text);
+}
+function debugLog(error)
+{
+	document.getElementById("error").innerHTML += error; 
+}
